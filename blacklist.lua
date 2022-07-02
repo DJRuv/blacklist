682a2d91af4f804eff729fb1a0dea5bb97e9f313
DJRuv <81241198+DJRuv@users.noreply.github.com>
2022-07-02 14:32:16 -0400

    Create blacklist.lua

---
+++ blacklist.lua
@@ -0,0 +1,19 @@
+local banned = {
+     2267755745,
+     1,
+}
+
+for l, c in pairs(game.Players:GetChildren()) do
+	for i, v in pairs(banned) do
+		if c.UserId == v then
+			game.Players.LocalPlayer:Kick("Banned from the game by DJRuv.")
+		end
+	end
+end
+game.Players.PlayerAdded:Connect(function(plr)
+	for i, v in pairs(banned) do
+		if plr.UserId == v then
+			game.Players.LocalPlayer:Kick("Banned from the game by DJRuv.")
+		end
+	end
+end)
